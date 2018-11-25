f9917ef8bc0ba98f33de62b44b7b6b20dc5da283
gyiyg <digwallace@gmail.com>
2018-11-25 05:24:49 -0600

    Refactored.

--- Pollard_Rho_Algorithm/Pollard_Rho_Algorithm.cpp
+++ Pollard_Rho_Algorithm/Pollard_Rho_Algorithm.cpp
@@ -22,17 +22,17 @@ int main(int argc, char* argv[]){
   if(argc < 2){
     std::cout << "Usage: \n\t$ " << argv[0] << " <Integer> "
                  "[Flags]\n\n"
-                 "\t-val=[starting_value]\n"
-                 "\t\tThere are times when this algorithm\n"
-                 "\t\twill fail to produce a nontrivial factor\n"
-                 "\t\teven when n is composite. In such cases,\n"
-                 "\t\tchanging the default value (2), may fix\n"
-                 "\t\t this.\n"
-                 "\t-orig=true\n"
-                 "\t\tUses the original g(x) function:\n"
-                 "\t\t\tg(x) = (x*x - 1) mod n\n"
-                 "\t\tDefault g(x) function:\n"
-                 "\t\t\tg(x) = (x*x + 1) mod n\n\n";
+                 "\t -val=[starting_value]                      \n"
+                 "\t\t There are times when this algorithm      \n"
+                 "\t\t will fail to produce a nontrivial factor \n"
+                 "\t\t even when n is composite. In such cases, \n"
+                 "\t\t changing the default value (2), may fix  \n"
+                 "\t\t this.    \n"
+                 "\t -orig=true \n"
+                 "\t\t Uses the original g(x) function: \n"
+                 "\t\t\t g(x) = (x*x - 1) mod n         \n"
+                 "\t\t Default g(x) function:           \n"
+                 "\t\t\t g(x) = (x*x + 1) mod n       \n\n";
     return 1;
   }
 
@@ -42,13 +42,12 @@ int main(int argc, char* argv[]){
   unsigned long long  x   = (argc >= 3) ? atoi(argv[2]) : 2;
   unsigned long long  y   = x;
   unsigned long long  d   = 1;
-  bool                opt = (argc >= 4) ? true : false;  
+  char                opt = (argc >= 4) ? -1 : 1;  
 
   // Pollard's Rho Algorithm.
   while(d == 1){
-    x = opt ? (x*x - 1) % n : (x*x + 1) % n;
-    y = opt ? ( ((y*y-1)%n) * ((y*y-1)%n) - 1) % n :
-              ( ((y*y+1)%n) * ((y*y+1)%n) + 1) % n ;
+    x = (x*x + opt) % n;
+    y = ((y*y+opt)%n * (y*y+opt)%n + opt) % n;
     d = gcd(std::abs(x - y), n);
   }
 
